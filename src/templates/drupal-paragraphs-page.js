30e4485b3883978c45ac8a7c5292b1a3970a753d
Krzyś <k.krzys@josera.pl>
2020-04-14 19:13:46 +0200

    feat: returned some barebone data for paragraphsPage template

--- src/templates/drupal-paragraphs-page.js
+++ src/templates/drupal-paragraphs-page.js
@@ -1,10 +1,28 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const DrupalParagraphsPage = () => {
+export const query = graphql`
+  query($url: String!) {
+    node: nodeParagraphsPage(path: { alias: { eq: $url } }) {
+      title
+      relationships {
+        paragraphs: field_paragraphs {
+          type: __typename
+        }
+      }
+    }
+  }
+`
+
+const DrupalParagraphsPage = ({ data: { node } }) => {
+  const paragraphs = node.relationships.paragraphs
   return (
     <Layout>
-      <h1>Drupal Paragraphs Page</h1>
+      <h1>{node.title}</h1>
+      {paragraphs.map(paragraph => (
+        <pre>{JSON.stringify(paragraph, null, 2)}</pre>
+      ))}
     </Layout>
   )
 }
